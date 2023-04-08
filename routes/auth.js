4be496735453fc79a4327916942e9c2ec8e0909c
ikponwosae <ikponwosae@gmail.com>
2023-04-08 01:46:30 +0100

    fix

--- routes/auth.js
+++ routes/auth.js
@@ -3,13 +3,14 @@ const router = express.Router()
 const { userRegister} = require('../validations/user.validator')
 
 
-const {login,registerUser, registerBusiness, completeRegistration, staffRegister} =  require('../controllers/auth')
+const {login,registerUser, registerBusiness, completeRegistration, staffRegister, getUser} =  require('../controllers/auth')
 
 router.post('/register', registerUser)
 router.get('/verification/:token', completeRegistration)
 router.post('/register/business/:userId', registerBusiness)
 router.post('/staff-verify/:token', staffRegister)
 router.post('/login', login)
+router.get('/user/:id', getUser)
 
 
 module.exports = router
