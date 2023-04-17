09f623305b92d43423cab850165cbed106a57a2a
ikponwosae <ikponwosae@gmail.com>
2023-04-17 05:51:15 +0100

    fix: patch appointment

--- routes/appointment.js
+++ routes/appointment.js
@@ -3,6 +3,6 @@ const router = express.Router()
 const { createAppointment, assignAndApprove } = require('../controllers/appointment')
 const {auth, authoRize} = require('../middleware/authentication')
 
-router.put('/:id/approve', auth, authoRize('shop-owner'), assignAndApprove)
+router.patch('/:id/approve', auth, authoRize('shop-owner'), assignAndApprove)
 
 module.exports = router
